06fa9e869b6b8be735e08f90ecbbd430e204703a
Vlad <vlad-golubev-2019@list.ru>
2023-01-31 13:01:22 +0300

    fixes sider functions and font fixes

--- slider.js
+++ slider.js
@@ -1,32 +1,30 @@
 let position = 0;
 const slidesToShow = 2;
 const slidesToScroll = 1;
-const container = document.querySelector(".slider__container");
-const track = document.querySelector(".slider__content");
-const btnLeft = document.querySelector(".button__left");
-const btnRight = document.querySelector(".button__right");
-const img = document.querySelectorAll(".slider__img");
+const sliderBlock = document.querySelector(".slider");
+const container = sliderBlock.querySelector(".slider__container");
+const track = sliderBlock.querySelector(".slider__content");
+const btnLeft = sliderBlock.querySelector(".button__left");
+const btnRight = sliderBlock.querySelector(".button__right");
+const img = sliderBlock.querySelectorAll(".slider__img");
 const imgCount = img.length;
 const imgWidth = img[1].offsetWidth + 34;
 const movePosition = slidesToScroll * imgWidth;
 
-
-console.log(imgWidth)
-btnRight.addEventListener("click", () => {
+const onClickBtnRight = () =>  {
     const imgLeft = imgCount - (Math.abs(position) + slidesToShow * imgWidth) / imgWidth;
     position -= imgLeft >= slidesToScroll ? movePosition : imgLeft * imgWidth;
     setPosition();
     checkBtns();
-});
-btnLeft.addEventListener("click", () => {
+};
+const onClickLeftRight = () => {
     const imgLeft = Math.abs(position)/ imgWidth;
     position += imgLeft >= slidesToScroll ? movePosition : imgLeft * imgWidth;
 
     setPosition();
     checkBtns();
-    console.log(position);
-});
-
+    
+};
 const setPosition = () => {
     track.style.transform = `translateX(${position}px)`;
 };
@@ -37,3 +35,7 @@ const checkBtns = () => {
 };
 
 checkBtns();
+btnRight.addEventListener("click", onClickBtnRight);
+btnLeft.addEventListener("click", onClickLeftRight);
+console.log(imgWidth);
+
