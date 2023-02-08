086de9b1d24507d44bf0c3c52012cfaec2c750ad
unknown <vlad-golubev-2019@list.ru>
2023-02-08 19:18:05 +0300

    adding custom slider buttons and adaptive fix

--- js/slider.js
+++ js/slider.js
@@ -1,51 +1,14 @@
-// let position = 0;
-// const slidesToShow = 2;
-// const slidesToScroll = 1;
-// const sliderBlock = document.querySelector(".slider");
-// const container = sliderBlock.querySelector(".slider__container");
-// const track = sliderBlock.querySelector(".slider__content");
-// const btnLeft = sliderBlock.querySelector(".button__left");
-// const btnRight = sliderBlock.querySelector(".button__right");
-// const img = sliderBlock.querySelectorAll(".slider__img");
-// const imgCount = img.length;
-// const imgWidth = img[1].offsetWidth + 34;
-// const movePosition = slidesToScroll * imgWidth;
-// const pageWidth = document.documentElement.scrollWidth;
-
-
-
-// const onClickBtnRight = () =>  {
-//     const imgLeft = imgCount - (Math.abs(position) + slidesToShow * imgWidth) / imgWidth;
-//     position -= imgLeft >= slidesToScroll ? movePosition : imgLeft * imgWidth;
-//     setPosition();
-//     checkBtns();
-// };
-
-// const onClickLeftRight = () => {
-//     const imgLeft = Math.abs(position)/ imgWidth;
-//     position += imgLeft >= slidesToScroll ? movePosition : imgLeft * imgWidth;
-
-//     setPosition();
-//     checkBtns();
-    
-// };
-
-// const setPosition = () => {
-//     track.style.transform = `translateX(${position}px)`;
-// };
-
-// const checkBtns = () => {
-//     btnLeft.disabled = position === 0;
-//     btnRight.disabled = position <= -(imgCount - slidesToShow) * imgWidth;
-
-// };
 jQuery (document).ready(($) => {
     $('.slider').owlCarousel({
         items:2,
         nav: false,
         dots: false,
-        margin:15,
+        pagination:false,
+        margin:0,
         responsiveClass:true,
+        navText:[
+            '<i class="fa-solid fa-arrow-left"></i>',
+            '<i class="fa-solid fa-arrow-right"></i>'],
         responsive:{
             0:{
                 items:1,
@@ -57,12 +20,17 @@ jQuery (document).ready(($) => {
                 nav:false
             },
             794:{
-                items:2,
+                items:1,
                 nav:false
             },
-            996:{
+            958:{
                 items:2,
-                nav:false
+                nav:false,
+            },
+            1000:{
+                items:2,
+                nav:true,
+                loop:false
             },
             1440:{
                 items:2,
@@ -73,8 +41,3 @@ jQuery (document).ready(($) => {
     
     });
 }) 
-
-// setPosition();
-// checkBtns();
-// btnRight.addEventListener("click", onClickBtnRight);
-// btnLeft.addEventListener("click", onClickLeftRight);
