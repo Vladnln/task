7258029e4a1028f5c999a6687101f50d28904bac
Vlad <vlad-golubev-2019@list.ru>
2023-02-09 16:56:03 +0300

    1

--- js/slider.js
+++ js/slider.js
@@ -1,43 +1,37 @@
-jQuery (document).ready(($) => {
-    $('.slider').owlCarousel({
-        items:2,
-        nav: false,
-        dots: false,
-        pagination:false,
-        margin:0,
-        responsiveClass:true,
-        navText:[
-            '<i class="fa-solid fa-arrow-left"></i>',
-            '<i class="fa-solid fa-arrow-right"></i>'],
-        responsive:{
-            0:{
-                items:1,
-                nav:false
-                
-            },
-            542:{
-                items:1,
-                nav:false
-            },
-            794:{
-                items:1,
-                nav:false
-            },
-            890:{
-                items:1,
-                nav:false
-            },
-            1000:{
-                items:2,
-                nav:true,
-                loop:false
-            },
-            1440:{
-                items:2,
-                nav:true,
-                loop:false
-            }
+const swiper = new Swiper ('.swiper', {
+    direction:'horizontal',
+    slidesPerView:2,
+    loop:false,
+    navigation: {
+        nextEl: '.custom-next',
+        prevEl: '.custom-prev',
+      },
+      breakpoints:{
+        1100:{
+          spaceBetween: -120
+        },
+        932:{
+          spaceBetween: -75
+        },
+        910:{
+          spaceBetween: -70
+        },
+        879:{
+          spaceBetween: -50
+        },
+        849:{
+          spaceBetween: -60
+        },
+        750:{
+          spaceBetween: -60
+        },
+        732:{
+          slidesPerView: 2
+        },
+        0:{
+          slidesPerView:1
         }
-    
-    });
-}) 
+      }
+})
+
+
