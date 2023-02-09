260280084e9ed04fb6f3b848eacd1af75c1d3041
unknown <vlad-golubev-2019@list.ru>
2023-02-09 22:05:50 +0300

    colorising arrows in buttons and styling buttons

--- js/slider.js
+++ js/slider.js
@@ -1,37 +1,35 @@
-const swiper = new Swiper ('.swiper', {
-    direction:'horizontal',
-    slidesPerView:2,
-    loop:false,
-    navigation: {
-        nextEl: '.custom-next',
-        prevEl: '.custom-prev',
-      },
-      breakpoints:{
-        1100:{
-          spaceBetween: -120
-        },
-        932:{
-          spaceBetween: -75
-        },
-        910:{
-          spaceBetween: -70
-        },
-        879:{
-          spaceBetween: -50
-        },
-        849:{
-          spaceBetween: -60
-        },
-        750:{
-          spaceBetween: -60
-        },
-        732:{
-          slidesPerView: 2
-        },
-        0:{
-          slidesPerView:1
-        }
-      }
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  slidesPerView: 2,
+  loop: false,
+  navigation: {
+    nextEl: '.custom-next',
+    prevEl: '.custom-prev',
+  },
+  breakpoints: {
+    1100: {
+      spaceBetween: -120
+    },
+    932: {
+      spaceBetween: -75
+    },
+    910: {
+      spaceBetween: -70
+    },
+    879: {
+      spaceBetween: -50
+    },
+    849: {
+      spaceBetween: -60
+    },
+    750: {
+      spaceBetween: -60
+    },
+    732: {
+      slidesPerView: 2
+    },
+    0: {
+      slidesPerView: 1
+    }
+  }
 })
-
-
