4a7f8975ba2dfbba70a27a73df71bf955b02eb81
Vlad <vlad-golubev-2019@list.ru>
2023-02-14 16:29:17 +0300

    fixing trash

--- js/calories-counter.js
+++ js/calories-counter.js
@@ -1,34 +1,35 @@
 const result = document.querySelector('.calculating__result');
 let gender, height, weight, age, ratio;
-
-function calculator() {
-    if (!gender || !height || !weight || !age || !ratio){
-        result.textContent='__';
+function calcTotal() {
+    if (!gender || !height || !weight || !age || !ratio) {
+        result.textContent = '__';
         return;
     }
-    if (gender === 'female'){
-        result.textContent = (447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio;
+    if (gender === 'female') {
+        result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
     } else {
-      result.textContent = (88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio;
+        result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
     }
-    console.log(ratio, gender)
-    elements.forEach(elem => {
-        elem.classList.remove(activeClass);
-    })
-    e.target.classList.add(activeClass);
 }
+calcTotal();
 function getStaticInformation(parentSelector, activeClass) {
     const elements = document.querySelectorAll(`${parentSelector} div`);
 
-    document.querySelector(parentSelector).addEventListener('click', (e) => {
-        if (e.target.getAttribute('data-ratio')){
-            ratio = +e.target.getAttribute('data-ratio');
-        } else {
-           gender = e.target.getAttribute('id');
-        }
+    elements.forEach(elem => {
+        elem.addEventListener('click', (e) => {
+            if (e.target.getAttribute('data-ratio')) {
+                ratio = +e.target.getAttribute('data-ratio');
+            } else {
+                gender = e.target.getAttribute('id');
+            }
+            console.log(ratio, gender)
+        })
     })
-}
 
+}
+getStaticInformation('#gender', 'calculating__choose-item_active');
+getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+    console.log(ratio, gender)
 function getInputInformation(selector) {
     const input = document.querySelector(selector);
     input.addEventListener('input', () => {
@@ -43,13 +44,9 @@ function getInputInformation(selector) {
                 age = +input.value;
                 break;
         }
-        calculator();
+        calcTotal();
     })
 }
 getInputInformation('#height');
 getInputInformation('#weight');
 getInputInformation('#age');
-getStaticInformation('#gender', 'calculating__choose-item_active');
-getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
-console.log(ratio, gender);
-calculator();
