88bb749726e15121f7a75bf688e0621658887e66
Vlad <vlad-golubev-2019@list.ru>
2023-03-27 15:49:54 +0300

    adding cart design

---
+++ src/js/cart.js
@@ -0,0 +1,7 @@
+const cartContainer = document.querySelector(".cart");
+const cartList = document.querySelector(".cart__list");
+const cartPlaceholder = document.querySelector(".cart__placeholder");
+const cardClose = document.querySelector(".cart__close");
+const cartOrderBtn = document.querySelector(".cart__order");
+const cartCleanBtn = document.querySelector(".cart__clean");
+const cartOpen = document.querySelector(".cart__open");
