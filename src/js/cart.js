4fae58c5a6b05b4e24955c165246c070ce1f5406
Vlad <vlad-golubev-2019@list.ru>
2023-04-05 17:30:16 +0300

    decision to rewrite cart scripts

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,70 +1,38 @@
 const cartContainer = document.querySelector(".cart");
 const cartHidden = document.querySelector(".cart__hidden");
+const body = document.querySelector("body");
 const cartList = document.querySelector(".cart__list");
 const cartPlaceholder = document.querySelector(".cart__placeholder");
 const cartClose = document.querySelector(".cart__close");
 const cartOrderBtn = document.querySelector(".cart__order");
 const cartCleanBtn = document.querySelector(".cart__clean");
 const cartOpen = document.querySelector(".cart__open");
+const btnPlus = document.querySelector('[data-action="plus"]');
+const btnMinus = document.querySelector('[data-action="minus"]');
+const floatingCartCounter = document.querySelector("[data-follow]");
+const counterTotal = document.querySelector(".cart__total-number");
+const itemPrice = document.querySelector("[data-price]");
+const dishOrderBtn = document.querySelector("[data-cart]");
 
-function openCartMenu() {
-	cartOpen.addEventListener("click", function () {
-		cartOpen.style.visibility = "hidden";
-		cartHidden.classList.toggle("cart__hidden");
-	});
+function handleCartOpen() {
+	cartHidden.classList.remove(".cart__hidden");
+	body.style = `overflow: hidden; padding-right: ${window.innerWidth - body.offsetWidth}px`;
+	popup.scrollTo(0, 0);
 }
 
-function closeCartMenu() {
-	cartClose.addEventListener("click", function () {
-		cartContainer.classList.add("cart__hidden");
-		cartOpen.style.visibility = "visible";
-	});
+function handleCartHide() {
+	cartHidden.classList.add("cart__hidden");
+	body.style = "overflow: auto;";
 }
 
-function updateCart() {
-	if (event.target.hasAttribute("data-cart")) {
-		floatingCartCounter.innerText = ++floatingCartCounter.innerText;
+function handleCounter(event) {
+	switch (event.target.dataset.action) {
+		case "incr":
+			counter.textContent = +counter.textContent + 1;
+			break;
+		case "decr":
+			if (+counter.textContent < 2) break;
+			counter.textContent = +counter.textContent - 1;
+			break;
 	}
 }
-
-openCartMenu();
-closeCartMenu();
-
-window.addEventListener("click", (event) => {
-	if (event.target.hasAttribute("data-cart")) {
-		const dishCard = event.target.closest(".dishes__list-item");
-
-		const prodInfo = {
-			id: dishCard.dataset.id,
-			imgSrc: dishCard.querySelector(".dishes__img").getAttribute("src"),
-			title: dishCard.querySelector(".dishes__title").innerText,
-			counter: 1,
-		};
-
-		const itemInCart = cartList.querySelector(`[data-id="${prodInfo.id}"]`);
-
-		if (itemInCart) {
-			const counterElement = itemInCart.querySelector("[data-counter]");
-			counterElement.innerText = parseInt(counterElement.innerText) + parseInt(prodInfo.counter);
-		} else {
-			const cartItemHtml = `
-			<div class="cart__list" data-id="${prodInfo.id}">
-				<div class="cart__item">
-					<div class="cart__item_img"><img src="${prodInfo.imgSrc}" alt="" /></div>
-					<div class="cart__item_title">${prodInfo.title}</div>
-					<div class="cart__item_price">
-						<div data-type="price">400</div>
-						<sup>&dollar;</sup>
-					</div>
-					<div class="cart__item_counter">
-						<button class="cart__button_plus" data-action="plus">+</button>
-						<div class="cart__item_couner-txt" data-counter>${prodInfo.counter}</div>
-						<button class="cart__button_minus" data-action="minus">-</button>
-					</div>
-				</div>
-			</div>`;
-			cartList.insertAdjacentHTML("beforeend", cartItemHtml);
-		}
-	}
-	updateCart();
-});
